66a1350c9c731e2ad138a82e6b4affbcaa806fb7
MS-Harine <21700371@handong.edu>
2020-05-05 21:32:41 +0900

    Add perks

--- killer/killer.js
+++ killer/killer.js
@@ -1,13 +1,14 @@
-var index = 0,
+var index = 1,
     amount = 0,
     currTransl = [],
     translationComplete = true,
     moveOffset = 0,
     amount_killer = 0;
 
-var transitionCompleted = function(){
-    translationComplete = true;
-}
+var i = 0, j = 0;
+var perk_set_status = [false, false, false, false];
+var perk_set_index = [-1, -1, -1, -1];
+var last_index = 0;
 
 var killer_list = [
   "IconPerks_bitterMurmur.png",
@@ -49,7 +50,7 @@ var killer_list = [
   "Teachable_hexDevourHope.png",
   "Teachable_hexHauntedGround.png",
   "Teachable_hexHuntressLullaby.png",
-  "Teachable_hexRetribution",
+  "Teachable_hexRetribution.png",
   "Teachable_hexRuin.png",
   "Teachable_hexTheThirdSeal.png",
   "Teachable_imAllEars.png",
@@ -81,59 +82,82 @@ var killer_list = [
   "Teachable_unnervingPresence.png",
 ].sort(function(){return 0.5-Math.random()});
 
-document.addEventListener("DOMContentLoaded", function(event)
-{
-    var carousel = document.getElementById('carousel');
+document.addEventListener("DOMContentLoaded", function(event) {
+  var carousel = document.getElementById('carousel');
 
-    amount_killer = killer_list.length;
-    for (var i = 0; i < amount_killer; i++) {
-      var child = document.createElement('li');
-      child.classList.add('slide', 'animate');
-      var img = document.createElement('img');
-      img.src = "../img/killer/" + killer_list[i];
-      child.appendChild(img);
-      carousel.appendChild(child);
-    }
+  amount_killer = killer_list.length;
+  for (i = 0; i < amount_killer; i++) {
+    var child = document.createElement('li');
+    child.classList.add('slide', 'animate');
+    var img = document.createElement('img');
+    img.src = "../img/killer/" + killer_list[i];
+    child.appendChild(img);
+    carousel.appendChild(child);
+  }
 
-    amount = document.getElementsByClassName("slide").length;
-    // get the width of the container
-    moveOffset = parseInt(window.getComputedStyle(document.getElementById('carousel-container')).width, 10);
-    // calcuate the width of the carousel
-    document.getElementById('carousel').style.width = (amount * moveOffset) + 'px';
-    // prevent multiple click when transition
-    for(var i = 0; i < amount; i++)
-        currTransl[i] = -moveOffset;
-    // add the last item to the start so that translateX(-moveOffset) works (In case the first click is the previous button)
-    // document.getElementById('carousel').insertBefore(document.getElementById('carousel').children[4],document.getElementById('carousel').children[0])
-    // add click events to control arrows
-    document.getElementById('next').addEventListener('click', next, true);
+  amount = document.getElementsByClassName("slide").length;
+  // get the width of the container
+  moveOffset = parseInt(window.getComputedStyle(document.getElementById('carousel-container')).width, 10);
+  // calcuate the width of the carousel
+  document.getElementById('carousel').style.width = (amount * moveOffset) + 'px';
+  // prevent multiple click when transition
+  for(i = 0; i < amount; i++)
+    currTransl[i] = -moveOffset;
 });
 
-function next()
-{
-    for(var i = 0; i < amount; i++)
-    {
-        var slide = document.getElementsByClassName("slide")[i];
-        slide.style.transform = 'translateX('+(currTransl[i]-moveOffset)+'px)';
-        currTransl[i] = currTransl[i]-moveOffset;
-    }
+function next() {
+  for(i = 0; i < amount; i++) {
+    var slide = document.getElementsByClassName("slide")[i];
+    slide.style.transform = 'translateX('+(currTransl[i]-moveOffset)+'px)';
+    currTransl[i] = currTransl[i]-moveOffset;
+  }
 
-    if (index >= 5) {
-        var outerIndex = (index - 5) % amount;
-        var outerSlide = document.getElementsByClassName("slide")[outerIndex];
-        outerSlide.style.opacity = 0;
-        outerSlide.style.transform = 'translateX('+(currTransl[outerIndex]+(moveOffset*amount))+'px)';
-        currTransl[outerIndex] = currTransl[outerIndex]+moveOffset*(amount);
-        outerSlide.style.opacity = 1;
-    }
-    index++;
+  if (index >= 5) {
+    var outerIndex = (index - 5) % amount;
+    var outerSlide = document.getElementsByClassName("slide")[outerIndex];
+    outerSlide.style.opacity = 0;
+    outerSlide.style.transform = 'translateX('+(currTransl[outerIndex]+(moveOffset*amount))+'px)';
+    currTransl[outerIndex] = currTransl[outerIndex]+moveOffset*(amount);
+    outerSlide.style.opacity = 1;
+  }
+  index++;
 }
 
 function roll() {
-    document.getElementsByClassName("slide")[(index + 1) % amount].children[0].style.opacity = 0.5;
-    var rand = parseInt(Math.random() * (amount_killer - 1)) + 1;
-    console.log(rand);
-    document.getElementsByClassName("slide")[(index + rand + 1) % amount].children[0].style.opacity = 1;
-    for (var j = 0; j < rand; j++)
-        setTimeout(next, j * 50);
+  document.getElementsByClassName("slide")[(index) % amount].children[0].style.opacity = 0.5;
+  var rand = parseInt(Math.random() * (amount_killer - 1)) + 1;
+  document.getElementsByClassName("slide")[(index + rand) % amount].children[0].style.opacity = 1;
+
+  while (perk_set_index.includes((index + rand) % amount))
+    rand = parseInt(Math.random() * (amount_killer - 1)) + 1;
+
+  setTimeout("set_perk_img(" + (index + rand) % amount + ")", rand * 50 + 500);
+  for (j = 0; j < rand; j++)
+    setTimeout(next, j * 50);
+}
+
+function set_perk_img(img_index) {
+  for (i = 0; i < 4; i++) {
+    if (perk_set_status[i] == false)
+      break;
+  }
+  if (i == 4) {
+    i = last_index % 4;
+    last_index++;
+  }
+
+  perk_set_index[i] = img_index;
+  perk_set_status[i] = true;
+  var img = document.getElementsByClassName("img_rand")[i];
+  img.src = "../img/killer/" + killer_list[img_index];
+  img.style.display = "block";
+}
+
+function reset_perk_img(perk_index) {
+  perk_set_status[perk_index] = false;
+  perk_set_index[perk_index] = -1;
+
+  var img = document.getElementsByClassName("img_rand")[perk_index];
+  img.style.display = "none";
+  img.src = "";
 }
--- surviver/surviver.js
+++ surviver/surviver.js
@@ -1,13 +1,14 @@
-var index = 0,
+var index = 1,
     amount = 0,
     currTransl = [],
     translationComplete = true,
     moveOffset = 0,
     amount_surviver = 0;
 
-var transitionCompleted = function(){
-    translationComplete = true;
-}
+var i = 0, j = 0;
+var perk_set_status = [false, false, false, false];
+var perk_set_index = [-1, -1, -1, -1];
+var last_index = 0;
 
 var surviver_list = [
   "IconPerks_anyMeansNecessary.png",
@@ -91,8 +92,7 @@ var surviver_list = [
   "Teachable_windowsOfOpportunity.png",
 ].sort(function(){return 0.5-Math.random()});
 
-document.addEventListener("DOMContentLoaded", function(event)
-{
+document.addEventListener("DOMContentLoaded", function(event) {
     var carousel = document.getElementById('carousel');
 
     amount_surviver = surviver_list.length;
@@ -113,37 +113,61 @@ document.addEventListener("DOMContentLoaded", function(event)
     // prevent multiple click when transition
     for(var i = 0; i < amount; i++)
         currTransl[i] = -moveOffset;
-    // add the last item to the start so that translateX(-moveOffset) works (In case the first click is the previous button)
-    // document.getElementById('carousel').insertBefore(document.getElementById('carousel').children[4],document.getElementById('carousel').children[0])
-    // add click events to control arrows
-    document.getElementById('next').addEventListener('click', next, true);
 });
 
-function next()
-{
-    for(var i = 0; i < amount; i++)
-    {
-        var slide = document.getElementsByClassName("slide")[i];
-        slide.style.transform = 'translateX('+(currTransl[i]-moveOffset)+'px)';
-        currTransl[i] = currTransl[i]-moveOffset;
-    }
+function next() {
+  for(var i = 0; i < amount; i++) {
+    var slide = document.getElementsByClassName("slide")[i];
+    slide.style.transform = 'translateX('+(currTransl[i]-moveOffset)+'px)';
+    currTransl[i] = currTransl[i]-moveOffset;
+  }
 
-    if (index >= 5) {
-        var outerIndex = (index - 5) % amount;
-        var outerSlide = document.getElementsByClassName("slide")[outerIndex];
-        outerSlide.style.opacity = 0;
-        outerSlide.style.transform = 'translateX('+(currTransl[outerIndex]+(moveOffset*amount))+'px)';
-        currTransl[outerIndex] = currTransl[outerIndex]+moveOffset*(amount);
-        outerSlide.style.opacity = 1;
-    }
-    index++;
+  if (index >= 5) {
+    var outerIndex = (index - 5) % amount;
+    var outerSlide = document.getElementsByClassName("slide")[outerIndex];
+    outerSlide.style.opacity = 0;
+    outerSlide.style.transform = 'translateX('+(currTransl[outerIndex]+(moveOffset*amount))+'px)';
+    currTransl[outerIndex] = currTransl[outerIndex]+moveOffset*(amount);
+    outerSlide.style.opacity = 1;
+  }
+  index++;
 }
 
 function roll() {
-    document.getElementsByClassName("slide")[(index + 1) % amount].children[0].style.opacity = 0.5;
+    document.getElementsByClassName("slide")[(index) % amount].children[0].style.opacity = 0.5;
     var rand = parseInt(Math.random() * (amount_surviver - 1)) + 1;
-    console.log(rand);
-    document.getElementsByClassName("slide")[(index + rand + 1) % amount].children[0].style.opacity = 1;
+    document.getElementsByClassName("slide")[(index + rand) % amount].children[0].style.opacity = 1;
+
+    while (perk_set_index.includes((index + rand) % amount))
+      rand = parseInt(Math.random() * (amount_killer - 1)) + 1;
+
+    setTimeout("set_perk_img(" + (index + rand) % amount + ")", rand * 50 + 500);
     for (var j = 0; j < rand; j++)
-        setTimeout(next, j * 50);
+      setTimeout(next, j * 50);
+}
+
+function set_perk_img(img_index) {
+  for (i = 0; i < 4; i++) {
+    if (perk_set_status[i] == false)
+      break;
+  }
+  if (i == 4) {
+    i = last_index % 4;
+    last_index++;
+  }
+
+  perk_set_index[i] = img_index;
+  perk_set_status[i] = true;
+  var img = document.getElementsByClassName("img_rand")[i];
+  img.src = "../img/surviver/" + surviver_list[img_index];
+  img.style.display = "block";
+}
+
+function reset_perk_img(perk_index) {
+  perk_set_status[perk_index] = false;
+  perk_set_index[perk_index] = -1;
+
+  var img = document.getElementsByClassName("img_rand")[perk_index];
+  img.style.display = "none";
+  img.src = "";
 }
